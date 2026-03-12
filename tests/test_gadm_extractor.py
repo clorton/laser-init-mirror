aaffff587f9f5017b417fc11ad778fbc12f71042
Christopher Lorton <christopher.lorton@gatesfoundation.org>
2026-03-12 09:21:39 -0400

    🧪 fix tests

--- tests/test_gadm_extractor.py
+++ tests/test_gadm_extractor.py
@@ -7,14 +7,14 @@ class TestGadmExtractor(unittest.TestCase):
     def test_gadm_extractor_geopackage(self):
         extractor = GadmExtractor(prefer_gpkg=True)
         local_file = extractor.extract("MCO", 1, 2025)
-        assert local_file.endswith("gadm41_MCO.gpkg")
+        assert str(local_file).endswith("gadm41_MCO.gpkg")
 
         return
 
     def test_Gadm_extractor_shapefile(self):
         extractor = GadmExtractor(prefer_gpkg=False)
         local_file = extractor.extract("MCO", 1, 2025)
-        assert local_file.endswith("gadm41_MCO_shp.zip")
+        assert str(local_file).endswith("gadm41_MCO_shp.zip")
 
         return
 
--- tests/test_geoboundaries_extractor.py
+++ tests/test_geoboundaries_extractor.py
@@ -7,10 +7,10 @@ class TestGeoBoundariesExtractor(unittest.TestCase):
     def test_geoboundaries_extractor(self):
         extractor = GeoBoundariesExtractor()
         local_file = extractor.extract("MCO", 1, 2025)
-        assert local_file.endswith("geoBoundaries-MCO-ADM1-all.zip")
+        assert str(local_file).endswith("geoBoundaries-MCO-ADM1-all.zip")
 
         local_file = extractor.extract("VAT", 0, 2025)
-        assert local_file.endswith("geoBoundaries-VAT-ADM0-all.zip")
+        assert str(local_file).endswith("geoBoundaries-VAT-ADM0-all.zip")
 
         return
 
